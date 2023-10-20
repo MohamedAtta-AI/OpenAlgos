abf6b02fb5c27266936d269df1d62ef602447c1b
MohamedAtta-AI <M.TarekSaad@nu.edu.eg>
2023-10-20 12:14:55 +0300

    b

--- functions.py
+++ functions.py
@@ -1,31 +1,3 @@
-def generate_fibonacci(n):
-    """
-    Print the first N numbers of the Fibonacci sequence
-
-    ### Parameters:
-    - n (int): The number of Fibonacci numbers to generate.
-
-    ### Example:
-    >>> generate_fibonacci(5)
-    0 1 1 2 3
-
-    ### Author:
-    Mohamed Atta
-    """
-    if not isinstance(n, int):
-        raise TypeError("Input must be an integer")
-    if n < 1:
-        raise ValueError("N must be positive")
-
-    a, b = 0, 1
-    for _ in range(n):
-        print(a, end=' ')
-        a, b = b, a + b
-
-
-N = eval(input("Enter the value of N: "))
-generate_fibonacci(N)
-
 def check_prime(n):
     """Check if a number is prime."""
     if n <= 1:
